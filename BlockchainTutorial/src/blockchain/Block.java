85b1eed7dca45221fc3e1699afad30d8401e2b81
Julian Malixi <malixijg@mcmaster.ca>
2018-06-10 21:02:55 -0400

    Constructor and computeHash() for Blocks

--- BlockchainTutorial/src/blockchain/Block.java
+++ BlockchainTutorial/src/blockchain/Block.java
@@ -1,5 +1,25 @@
 package blockchain;
 
+import java.util.Date;
+
+import util.StringHasher;
+
 public class Block {
 
+	public String currentHash;
+	public String previousHash;
+	private String data;
+	private long time;
+	
+	public Block(String data, String prevHash) {
+		this.data = data;
+		this.previousHash = prevHash;
+		this.time = new Date().getTime();
+		this.currentHash = computeHash();
+	}
+	
+	public String computeHash() {
+		String hash = StringHasher.sha256ify(previousHash + Long.toString(time) + data);
+		return hash;
+	}
 }
